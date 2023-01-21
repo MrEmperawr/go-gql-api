d7ea6e2702acd36a54b32bc18c998dade06c4767
Marcus Kalmar <marcus@videvit.se>
2023-01-21 16:01:42 +0100

    update schema and readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # go-gql-api
-Learning Golang with GQL
+This is a small Golang project aimed to showcase Go with GraphQL (gql).
+
+In this project we generate models via gql schemas.
+
+Queries and mutations to interact with the game and movie database can be found by running `go run server.go`.
+
+Happy graphing!
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,14 +11,14 @@ import (
 	"github.com/mremperawr/golang-graphql/graph/model"
 )
 
-// CreateTodo is the resolver for the createTodo field.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+// CreateMovie is the resolver for the createMovie field.
+func (r *mutationResolver) CreateMovie(ctx context.Context, input model.NewMovie) (*model.Movie, error) {
+	panic(fmt.Errorf("not implemented: CreateMovie - createMovie"))
 }
 
-// Todos is the resolver for the todos field.
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+// Movies is the resolver for the movies field.
+func (r *queryResolver) Movies(ctx context.Context) ([]*model.Movie, error) {
+	panic(fmt.Errorf("not implemented: Movies - movies"))
 }
 
 // Mutation returns MutationResolver implementation.
